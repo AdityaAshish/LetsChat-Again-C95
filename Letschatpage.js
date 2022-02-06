b6b6575b3f350ef1787e9433c410344bc7634e4a
AdityaAshish <81067181+AdityaAshish@users.noreply.github.com>
2022-02-06 10:31:49 +0530

    Add files via upload

---
+++ Letschatpage.js
@@ -0,0 +1,30 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+    apiKey: "AIzaSyD9mQnBm4O40C_CIXF89rBXVFceFRFi7Lw",
+    authDomain: "kwitter-bc148.firebaseapp.com",
+    databaseURL: "https://kwitter-bc148-default-rtdb.firebaseio.com",
+    projectId: "kwitter-bc148",
+    storageBucket: "kwitter-bc148.appspot.com",
+    messagingSenderId: "200000766482",
+    appId: "1:200000766482:web:5f43d11ae57771eec5cecb"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+       firebase_message_id = childKey;
+       message_data = childData;
+//Start code
+
+//End code
+    } });  }); }
+getData();
+
+
+
+function logout() {
+    localStorage.removeItem("user name");
+    localStorage.removeItem("roomname");
+    window.location.replace("LetsChatLogin.html");
+
+}
